fcd8759f3314f5015853ae09482b7b6c5d1d1fb7
dansola <soladbs@gmail.com>
2020-04-22 18:19:07 -0400

    started server & client

---
+++ src/waypoint_client.cpp
@@ -0,0 +1,38 @@
+#include "ros/ros.h"
+#include "actionlib/client/simple_action_client.h"
+#include "software_challenge/waypointAction.h"
+
+int main (int argc, char **argv) {
+    ros::init(argc, argv, "test_waypoint");
+
+    actionlib::SimpleActionClient<software_challenge::waypointAction> ac("waypoint", true);
+
+    ROS_INFO("Waiting for waypoint action server to start...");
+    ac.waitForServer();
+
+    ROS_INFO("Waypoint action server started.  Where do you want the moving turtle to go?");
+
+    float x, y;
+    std::cout << "X Coordinate:";
+    std::cin >> x;
+    std::cout << "Y Coordinate:";
+    std::cin >> y;
+
+    software_challenge::waypointGoal goal;
+    goal.x = x;
+    goal.y = y;
+
+    ac.sendGoal(goal);
+
+    bool finished_before_timeout = ac.waitForResult(ros::Duration(30.0));
+
+    if (finished_before_timeout) {
+        ROS_INFO("Succesfully moved the turtle to x=%f, y=%f", x, y);
+    }
+    else {
+        ROS_INFO("Turtle did not move to x=%f, y=%f before the time out.", x, y);
+    }
+
+    return 0;
+}
+
--- src/waypoint_server.cpp
+++ src/waypoint_server.cpp
@@ -1,4 +1,35 @@
+#include "ros/ros.h"
+#include "actionlib/server/simple_action_server.h"
+#include "software_challenge/waypointAction.h"
+
 class waypointAction {
+
+    protected:
+
+        ros::NodeHandle nh_;
+        actionlib::SimpleActionServer<software_challenge::waypointAction> as_;
+
+        std::string action_name_;
+
+        software_challenge::waypointActionFeedback feedback_;
+        software_challenge::waypointActionResult result_;
+
+    public:
+
+        waypointAction(std::string name) :
+            as_(nh_, name, boost::bind(&waypointAction::executeCB, this, _1), false),
+            action_name_(name) {
+
+            as_.start();
+        }
+
+        ~waypointAction(void) {
+        }
+
+        void executeCB(const software_challenge::waypointGoalConstPtr &goal) {
+            
+        }
+
     
 };
 
